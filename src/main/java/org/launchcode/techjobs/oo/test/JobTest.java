9ddc073d897c5f6c015458223e4e8a1f95537e2a
Andrej <138933791+Andrej-Clarey@users.noreply.github.com>
2023-08-31 15:58:28 -0400

    task4comp

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -25,6 +25,25 @@ public class JobTest {
                 PositionType("Quality control"), new CoreCompetency("Persistence"));
         assertTrue(testJob3.getName() instanceof String);
         assertEquals("Product tester", testJob3.getName());
+        assertTrue(testJob3.getEmployer() instanceof Employer);
+        assertEquals("ACME", testJob3.getEmployer().getValue());
+        assertTrue(testJob3.getLocation() instanceof Location);
+        assertEquals("Desert",testJob3.getLocation().getValue());
+        assertTrue(testJob3.getPositionType() instanceof PositionType);
+        assertEquals("Quality control", testJob3.getPositionType().getValue());
+        assertTrue(testJob3.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals("Persistence", testJob3.getCoreCompetency().getValue());
     }
+
+    @Test
+    public void testJobsForEquality(){
+        Job testJob4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new
+                PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job testJob5 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new
+                PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertFalse(testJob4 == testJob5);
+    }
+
+
 
 }
